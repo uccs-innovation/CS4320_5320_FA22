abc1ff6c69bdbe99d0babf7b48ae46cf79295205
David Schendt <david.schendt@gmail.com>
2022-09-18 23:56:00 -0600

    Added Checkbox and playing with UI

--- StudyN/Models/Item.cs
+++ StudyN/Models/Item.cs
@@ -2,6 +2,7 @@
 {
     public class Item
     {
+        public Boolean Completed { get; set; }
         public string Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
--- StudyN/Services/MockDataStore.cs
+++ StudyN/Services/MockDataStore.cs
@@ -9,14 +9,16 @@ namespace StudyN.Services
         public MockDataStore()
         {
             DateTime baseDate = DateTime.Today;
-            this.items = new List<Item>() {
-            new Item { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description.", StartTime = baseDate.AddHours(1), EndTime = baseDate.AddHours(2), Value=17.098 },
-            new Item { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description.", StartTime = baseDate.AddHours(2), EndTime = baseDate.AddHours(4), Value=9.985 },
-            new Item { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description.", StartTime = baseDate.AddHours(3), EndTime = baseDate.AddHours(5), Value=9.597},
-            new Item { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description.", StartTime = baseDate.AddHours(5), EndTime = baseDate.AddHours(6), Value=9.834 },
-            new Item { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description.", StartTime = baseDate.AddHours(9), EndTime = baseDate.AddHours(12), Value=3.287 },
-            new Item { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description.", StartTime = baseDate.AddHours(12), EndTime = baseDate.AddHours(15), Value=81.2 }
-        };
+            this.items = new List<Item>() 
+            {
+                new Item { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description.", StartTime = baseDate.AddHours(1), EndTime = baseDate.AddHours(2), Value=17.098 },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description.", StartTime = baseDate.AddHours(2), EndTime = baseDate.AddHours(4), Value=9.985 },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description.", StartTime = baseDate.AddHours(3), EndTime = baseDate.AddHours(5), Value=9.597},
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description.", StartTime = baseDate.AddHours(5), EndTime = baseDate.AddHours(6), Value=9.834 },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description.", StartTime = baseDate.AddHours(9), EndTime = baseDate.AddHours(12), Value=3.287 },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description.", StartTime = baseDate.AddHours(12), EndTime = baseDate.AddHours(15), Value=81.2 },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Seventh item", Description="This is an item description.", StartTime = baseDate.AddHours(25), EndTime = baseDate.AddHours(15), Value=81.2 }
+            };
         }
 
         public async Task<bool> AddItemAsync(Item item)
