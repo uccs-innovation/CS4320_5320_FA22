2ebba26d75ff05b5eda6bcd1686e1295d7af6196
Mholubec3 <mholubec@uccs.edu>
2022-09-21 23:25:54 -0600

    Added button functionality

--- StudyN/Views/CalendarPage.xaml.cs
+++ StudyN/Views/CalendarPage.xaml.cs
@@ -1,31 +1,47 @@
-﻿using StudyN.ViewModels;
+﻿using DevExpress.Maui.Scheduler;
+using DevExpress.Utils.Serializing;
+using DevExpress.XamarinAndroid.Scheduler.Visual.Data;
+using StudyN.ViewModels;
+using System.ComponentModel;
 
 namespace StudyN.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
+
+
     public partial class CalendarPage : ContentPage
     {
         public CalendarPage()
         {
+            
             InitializeComponent();
             BindingContext = ViewModel = new CalendarViewModel();
         }
 
         CalendarViewModel ViewModel { get; }
 
+
         void OnDailyClicked(object sender, EventArgs args)
         {
-            System.Diagnostics.Debug.WriteLine("test1");
+            dayView.IsVisible = true;
+            weekView.IsVisible = false;
+            monthView.IsVisible = false;
+
         }
 
         void OnWeeklyClicked(object sender, EventArgs args)
         {
-            System.Diagnostics.Debug.WriteLine("test2");
+            dayView.IsVisible = false;
+            weekView.IsVisible = true;
+            monthView.IsVisible = false;
+
         }
 
         void OnMonthlyClicked(object sender, EventArgs args)
         {
-            System.Diagnostics.Debug.WriteLine("test3");
+            dayView.IsVisible = false;
+            weekView.IsVisible = false;
+            monthView.IsVisible = true;
         }
 
         protected override void OnAppearing()
