d4c09a366355567a1d38990158bc5681a8fc0b70
David Schendt <david.schendt@gmail.com>
2022-09-25 12:14:28 -0600

    Update Fields

--- StudyN/Models/CalendarTask.cs
+++ StudyN/Models/CalendarTask.cs
@@ -4,56 +4,56 @@ namespace StudyN.Models
 {
     public class CalendarTask
     {
-        public CalendarTask(string Text)
+        public CalendarTask(string Text, DateTime DueTime)
         {
             this.Name = Text;
+            this.DueTime = DueTime;
         }
 
-        public bool Completed { get; set; }
-        public int Id { get; set; }
+        public bool Completed { get; set; } = false;
+        public Guid TaskId { get; set; } = Guid.NewGuid();
         public string Name { get; set; }
-        public string Description { get; set; }
-        public DateTime DueDate { get; set; }
-        public int TimeNeeded { get; set; }
-
-        public CalendarTasksData Parent { get; set; }
+        public string Description { get; set; } = "";
+        public DateTime DueTime { get; set; }
+        public int CompletionProgress { get; set; } = 0;
+        public int TotalTimeNeeded { get; set; } = 0;
     }
 
     public class CalendarTasksData
     {
-        void GenerateCalendarTaskss()
+        void GenerateCalendarTasks()
         {
             CalendarTasks.Add(
-                new CalendarTask("HW: Pitch your Application Idea")
+                new CalendarTask("HW: Pitch your Application Idea", DateTime.Today)
                 {
-                    Parent = this,
-                    Completed = false,
-                    Id = 1,
-                    Description = "Pitch your appilcation idea...",
-                    DueDate = DateTime.Today,
-                    TimeNeeded = 3
+                    //Parent = this,
+                    //Completed = false,
+                    //Id = 1,
+                    //Description = "Pitch your appilcation idea...",
+                    //DueDate = DateTime.Today,
+                    //TimeNeeded = 3
                 }
             ); ;
             CalendarTasks.Add(
-                new CalendarTask("HW: Technology Proof of Concept")
+                new CalendarTask("HW: Technology Proof of Concept", DateTime.Today)
                 {
-                    Parent = this,
-                    Completed = false,
-                    Id = 2,
-                    Description = "Prove your technology works...",
-                    DueDate = DateTime.Today,
-                    TimeNeeded = 7
+                    //Parent = this,
+                    //Completed = false,
+                    //Id = 2,
+                    //Description = "Prove your technology works...",
+                    //DueDate = DateTime.Today,
+                    //TimeNeeded = 7
                 }
             );
             CalendarTasks.Add(
-                new CalendarTask("HW: Prototype of Key Features")
+                new CalendarTask("HW: Prototype of Key Features", DateTime.Today)
                 {
-                    Parent = this,
-                    Completed = false,
-                    Id = 3,
-                    Description = "Build a prototype of the feature...",
-                    DueDate = DateTime.Today.AddHours(24),
-                    TimeNeeded = 5
+                    //Parent = this,
+                    //Completed = false,
+                    //Id = 3,
+                    //Description = "Build a prototype of the feature...",
+                    //DueDate = DateTime.Today.AddHours(24),
+                    //TimeNeeded = 5
                 }
             );
         }
@@ -72,7 +72,7 @@ namespace StudyN.Models
         {
             CalendarTasks = new ObservableCollection<CalendarTask>();
             CompletedTasks = new ObservableCollection<CalendarTask>();
-            GenerateCalendarTaskss();
+            GenerateCalendarTasks();
         }
     }
 }
