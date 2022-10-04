8a7dd0de3eeab8b1f9a49c28e20b9bc62df8cfa4
Emi-B-Dazed <81595382+Emi-B-Dazed@users.noreply.github.com>
2022-10-04 00:08:04 -0600

    Changed ObservableCollection<ListTask> name from CalendarTasks to ListTasks

--- StudyN/Models/ListTask.cs
+++ StudyN/Models/ListTask.cs
@@ -44,30 +44,30 @@ namespace StudyN.Models
         {
             ListTask newTask = new ListTask(name, description, dueTime, priority, CompletionProgress, TotalTimeNeeded);
             newTask.Parent = this;
-            CalendarTasks.Add(newTask);
+            ListTasks.Add(newTask);
             return newTask;
         }
 
         public void RemoveTask(Guid taskId)
         {
-            foreach(ListTask task in CalendarTasks)
+            foreach(ListTask task in ListTasks)
             {
                 if(task.TaskId == taskId)
                 {
-                    CalendarTasks.Remove(task);
+                    ListTasks.Remove(task);
                     return;
                 }
             }
         }
         public void CompleteTask(Guid taskId)
         {
-            foreach (ListTask task in CalendarTasks)
+            foreach (ListTask task in ListTasks)
             {
                 if (task.TaskId == taskId)
                 {
                     task.Completed = true;
                     CompletedTasks.Add(task);
-                    CalendarTasks.Remove(task);
+                    ListTasks.Remove(task);
                     return;
                 }
             }
@@ -75,22 +75,22 @@ namespace StudyN.Models
 
         public void DeleteTask(Guid taskId)
         {
-            foreach (ListTask task in CalendarTasks)
+            foreach (ListTask task in ListTasks)
             {
                 if (task.TaskId == taskId)
                 {
-                    CalendarTasks.Remove(task);
+                    ListTasks.Remove(task);
                     return;
                 }
             }
         }
 
-        public ObservableCollection<ListTask> CalendarTasks { get; private set; }
+        public ObservableCollection<ListTask> ListTasks { get; private set; }
         private ObservableCollection<ListTask> CompletedTasks { get; set; }
 
         public ListTaskData()
         {
-            CalendarTasks = new ObservableCollection<ListTask>();
+            ListTasks = new ObservableCollection<ListTask>();
             CompletedTasks = new ObservableCollection<ListTask>();
             GenerateCalendarTasks();
             UIGlobal.MainData = this;
