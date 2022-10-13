f0df4da5f7042a3f93a2a2e441595d6736bc8f0b
Joshue Torres Azanza <josuetorresf2@gmail.com>
2022-10-13 12:41:01 -0600

    changes from time bug (fixed)

--- StudyN/Views/AddTaskPage.xaml.cs
+++ StudyN/Views/AddTaskPage.xaml.cs
@@ -55,12 +55,15 @@ public partial class AddTaskPage : ContentPage
         this.description.Text = this.description.Text == null ? "" : this.description.Text;
         int timeLogged = this.tSpent.Value == null ? 0 : (int)this.tSpent.Value;
         int totalTime = this.tComplete.Value == null ? 0 : (int)this.tComplete.Value;
+
+        DateTime dateTime = new DateTime(this.date.Date.Value.Year, this.date.Date.Value.Month, this.date.Date.Value.Day,
+            this.time.Time.Value.Hour, this.time.Time.Value.Minute, this.time.Time.Value.Second);
         double percent = 0;
     
         UIGlobal.MainData.AddTask(
             this.name.Text,
             this.description.Text,
-            this.date.Date.Value.AddMilliseconds(this.time.Time.Value.TimeOfDay.TotalMilliseconds),
+            dateTime,
             (int)this.priority.Value,
             timeLogged,
             totalTime, 
