b38973dfc33bffe3afcc641cdb10b1e5ff4a9071
Joshue <josuetorresf2@gmail.com>
2022-10-27 13:46:52 -0600

    fixing error of group 1

--- StudyN/Views/AddTaskPage.xaml.cs
+++ StudyN/Views/AddTaskPage.xaml.cs
@@ -79,10 +79,45 @@ public partial class AddTaskPage : ContentPage
         DateTime dateTime = new DateTime(this.date.Date.Value.Year, this.date.Date.Value.Month, this.date.Date.Value.Day,
             this.time.Time.Value.Hour, this.time.Time.Value.Minute, this.time.Time.Value.Second);
 
+        TaskItem task;
+
         //Check to see if we are currently editing or adding a task
         if (editingExistingTask)
         {
-            HandleEditRecurrence(GlobalTaskData.ToEdit.TaskId);
+           
+            //Gets task list
+            ObservableCollection<TaskItem> taskList = new ObservableCollection<TaskItem>();
+            for (int i = 0; i < taskList.Count; i++)
+            {
+                //If information is not the same, then it gets saved for all
+                if (taskList[i].Description != this.description.Text)
+                {
+                    taskList[i].Description = this.description.Text;
+                }
+                if (taskList[i].DueTime != dateTime)
+                {
+                    taskList[i].DueTime = dateTime;
+                }
+                if (taskList[i].CompletionProgress != timeLogged)
+                {
+                    taskList[i].CompletionProgress = timeLogged;
+                }
+                if (taskList[i].TotalTimeNeeded != totalTime)
+                {
+                    taskList[i].TotalTimeNeeded = totalTime;
+                }
+            }
+            //Saves the informatiom when editing
+            GlobalTaskData.TaskManager.EditTask(
+                GlobalTaskData.ToEdit.TaskId,
+                this.name.Text,
+                this.description.Text,
+                dateTime,
+                (int)this.priority.Value,
+                timeLogged,
+                totalTime);
+            task = GlobalTaskData.ToEdit;
+            GlobalTaskData.ToEdit = null;
         }
         else
         {
@@ -200,46 +235,4 @@ public partial class AddTaskPage : ContentPage
 
         }
     }
-    void HandleEditRecurrence(Guid id)
-    {
-        int timeLogged = this.tSpent.Value == null ? 0 : (int)this.tSpent.Value;
-        int totalTime = this.tComplete.Value == null ? 0 : (int)this.tComplete.Value;
-        DateTime dateTime = new DateTime(this.date.Date.Value.Year, this.date.Date.Value.Month, this.date.Date.Value.Day,
-            this.time.Time.Value.Hour, this.time.Time.Value.Minute, this.time.Time.Value.Second);
-
-        //Gets task list
-        ObservableCollection<TaskItem> task = new ObservableCollection<TaskItem>();
-        for (int i = 0; i < task.Count; i++)
-        {
-            //If information is not the same, then it gets saved for all
-            if (task[i].Description != this.description.Text)
-            {
-                task[i].Description =this.description.Text;
-            }
-            if(task[i].DueTime != dateTime)
-            {
-                task[i].DueTime = dateTime;
-            }
-            if(task[i].CompletionProgress != timeLogged)
-            {
-                task[i].CompletionProgress = timeLogged;
-            }
-            if (task[i].TotalTimeNeeded != totalTime)
-            {
-                task[i].TotalTimeNeeded = totalTime;
-            }
-
-        }
-        //Saves the informatiom when editing
-        GlobalTaskData.TaskManager.EditTask(
-            GlobalTaskData.ToEdit.TaskId,
-            this.name.Text,
-            this.description.Text,
-            dateTime,
-            (int)this.priority.Value,
-            timeLogged,
-            totalTime);
-        GlobalTaskData.ToEdit = null;
-    }
-
 }
