775035c2ed40864abf9331042e0e686680fee486
Nick Petik <thelaugher1@gmail.com>
2022-10-26 19:25:39 -0700

    Able to create category

--- StudyN/Views/AddCategoryPage.xaml.cs
+++ StudyN/Views/AddCategoryPage.xaml.cs
@@ -1,5 +1,6 @@
 using StudyN.ViewModels;
 using DevExpress.XtraEditors;
+using StudyN.Models;
 
 namespace StudyN.Views
 {
@@ -10,6 +11,36 @@ namespace StudyN.Views
 			InitializeComponent();
 			BindingContext = new AddCategoryViewModel();
 			Title = "Add Category";
+			RemoveButton.IsVisible = false;
+		}
+
+		/// <summary>
+		/// Either creates a new category or make changes to existing category
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private async void SaveButtonClicked(object sender, EventArgs e)
+		{
+			this.name.Text = this.name.Text == null ? "No Name" : this.name.Text;
+			// if input is invalid than make color black
+			if (this.color.Text == null)
+			{
+				this.color.Text = "#000000";
+			}
+			else if (!this.color.Text.StartsWith('#'))
+			{
+				this.color.Text = "#000000";
+			}
+			else
+			{
+				this.color.Text = this.color.Text.Substring(7);
+			}
+			// turns color string input into color
+			Color useColor = Color.FromArgb(this.color.Text);
+			// creates the category
+			AppointmentCategory cat;
+			cat = GlobalAppointmentData.CalendarManager.CreateCategory(this.name.Text, useColor);
+			await Shell.Current.GoToAsync("..");
 		}
 	}
 }
