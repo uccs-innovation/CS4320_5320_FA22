ecdd2eb3cc1ef0a80cb2255d6e9bceec9b67458f
Kaleb Chanza <kalebchanza@gmail.com>
2022-11-03 20:16:45 -0600

    Merge conflict fix

--- StudyN/App.xaml.cs
+++ StudyN/App.xaml.cs
@@ -28,6 +28,7 @@ namespace StudyN
             Routing.RegisterRoute(nameof(SettingsPage), typeof(SettingsPage));
             Routing.RegisterRoute(nameof(AddEventPage), typeof(AddEventPage));
             Routing.RegisterRoute(nameof(TaskPage), typeof(TaskPage));
+            
             Routing.RegisterRoute(nameof(TaskChartsPage), typeof(TaskChartsPage));
             CrossFirebasePushNotification.Current.OnTokenRefresh += Current_OnTokenRefresh;
         }
