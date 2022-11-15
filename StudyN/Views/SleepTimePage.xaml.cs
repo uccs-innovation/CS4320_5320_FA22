d73caac497997a35c8f86a805fd14e466356733e
Nick Petik <thelaugher1@gmail.com>
2022-11-14 23:53:46 -0800

    Made layout of Sleep Time Page

--- StudyN/Views/SleepTimePage.xaml.cs
+++ StudyN/Views/SleepTimePage.xaml.cs
@@ -9,4 +9,15 @@ public partial class SleepTimePage : ContentPage
 		InitializeComponent();
 		BindingContext = new SleepTimeViewModel();
 	}
+
+	/// <summary>
+	/// When save button is clicked, save the input and go back to calendar page
+	/// </summary>
+	/// <param name="sender"></param>
+	/// <param name="e"></param>
+	private async void OnSaveButtonTap(object sender, EventArgs e)
+	{
+		Routing.RegisterRoute(nameof(Views.CalendarPage), typeof(Views.CalendarPage));
+		await Shell.Current.GoToAsync("..");
+	}
 }
