fc5cd4bda706dd134b9792129c7dd9ffdb71c474
shrey <shreyamadiga@gmail.com>
2022-11-17 12:17:45 -0700

    Added logo, removed some ambiguity

--- StudyN/Views/AboutUs.xaml.cs
+++ StudyN/Views/AboutUs.xaml.cs
@@ -8,27 +8,7 @@ public partial class AboutUs: ContentPage
     }
     public async void Button_clicked(object sender, EventArgs args)
     {
-        await Navigation.PushAsync(new HomePage());
+        await Shell.Current.GoToAsync("//HomePage");
     }
-    public async void Button_clicked1(object sender, EventArgs args)
-    {
-        await Navigation.PushAsync(new CalendarPage());
-    }
-    public async void Button_clicked2(object sender, EventArgs args)
-    {
-        await Navigation.PushAsync(new TaskPage());
-    }
-    public async void Button_clicked3(object sender, EventArgs args)
-    {
-        await Navigation.PushAsync(new AnalyticsPage());
-    }
-    public async void Button_clicked4(object sender, EventArgs args)
-    {
-        await Navigation.PushAsync(new CategoriesPage());
-    }
-    public async void Button_clicked5(object sender, EventArgs args)
-    {
-        await Navigation.PushAsync(new SettingsPage());
-    }
-
+    
 }
