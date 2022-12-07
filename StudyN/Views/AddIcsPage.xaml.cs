a17bfbf4265fd9db250514bea29811f8894ee564
Cameron Livingston <89604510+KcLivings@users.noreply.github.com>
2022-12-07 14:32:14 -0700

    updated ICS converting with Gabriel's changes, adds to task page, throws warning on successful finish and to tell user to change estimated times and priority
    basically, all of #454 and started #455.
    and improved ICS file reading dramatically and now works

--- StudyN/Views/AddIcsPage.xaml.cs
+++ StudyN/Views/AddIcsPage.xaml.cs
@@ -1,8 +1,6 @@
 
 /*
  * 
- * WHAT IS HAPPENING HERE??:
- * 
  * This takes in string inputted into text box
  * then pings https to get response of what is in https
  * returns as string to be parsed
@@ -21,6 +19,9 @@ using StudyN.Models;
 using StudyN.Utilities;
 using StudyN.ViewModels;
 using System.Net;
+using System;
+using System.Globalization;
+using DevExpress.DataAccess.Native.Sql.MasterDetail;
 
 namespace StudyN.Views
 {
@@ -52,12 +53,18 @@ namespace StudyN.Views
                 var content1 = client.GetStringAsync(link);
                 content = content1.Result;
                 Console.WriteLine(content1.Result);
-                await Shell.Current.GoToAsync(nameof(DisplayIntegratedCalPage));
+
+                GetAppointFromString convert = new GetAppointFromString(content);
+
+                
+
+
+                //await Shell.Current.GoToAsync(nameof(DisplayIntegratedCalPage));
 
             }
         }
 
-
+        
 
         private void Entry_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -72,11 +79,17 @@ namespace StudyN.Views
         //break massive string to individual appointments
         class GetAppointFromString
         {
+            CultureInfo enUS = new CultureInfo("en-US");
+            Color color;
+            //Color.FromArgb("#000000")
             private string line;
             private int id;
             private string name;
+            private string descript;
             private DateTime start = new DateTime();
+            private TimeSpan startTime = new TimeSpan();
             private DateTime end = new DateTime();
+            private TimeSpan endTime = new TimeSpan();
             private DateTime zdate = new DateTime();
             private TimeSpan duration = new TimeSpan();
             //private TimeSpan duration;
@@ -109,6 +122,11 @@ namespace StudyN.Views
                         line = line.Substring(8);
                         name = line;
                     }
+                    if (line.Contains("DESCRIPTION") == true)
+                    {
+                        line = line.Substring(12);
+                        descript = line;
+                    }
                     if (line.Contains("UID:"))
                     {
                         int last = line.LastIndexOf("-");
@@ -124,89 +142,60 @@ namespace StudyN.Views
                     }
                     if (line.Contains("DTSTART"))
                     {
-                        //set some variables
-                        int year = 0;
-                        int month = 0;
-                        int day = 0;
-                        int hour = 0;
-                        int minute = 0;
-                        int second = 0;
-
-                        //find what kind of dstart
+                        
                         int last = line.LastIndexOf(":");
                         line = line.Substring(last + 1);
 
-                        //get date
-                        string temp = line.Substring(0, 4);
-                        year = Convert.ToInt32(temp);
-                        line = line.Substring(4);
-                        temp = line.Substring(0, 2);
-                        month = Convert.ToInt32(temp);
-                        line = line.Substring(2);
-                        temp = line.Substring(0, 2);
-                        day = Convert.ToInt32(temp);
-
+                        //find what kind of dstart
                         if (line.Contains('T'))
                         {
-                            line = line.Substring(3);
+                            //get date
+                            string temp = line.Substring(0, 8);
+                            DateTime.TryParseExact(temp, "yyyyMMdd", enUS, DateTimeStyles.None, out start);
+
+                            line = line.Substring(9);
 
                             //get time
-                            temp = line.Substring(0, 2);
-                            hour = Convert.ToInt32(temp);
-                            line = line.Substring(2);
-                            temp = line.Substring(0, 2);
-                            minute = Convert.ToInt32(temp);
-                            line = line.Substring(2);
-                            temp = line.Substring(0, 2);
-                            second = Convert.ToInt32(temp);
+                            temp = line.Substring(0, 6);
+                            if (temp.Contains('Z'))
+                            {
+                                temp = line.Substring(0, 5);
+                                temp = "0" + temp;
+                            }
+                            TimeSpan.TryParseExact(temp, @"hmmss", CultureInfo.InvariantCulture, TimeSpanStyles.None, out startTime);
 
                             //insert into datetime
-                            start = new DateTime(year, month, day, hour, minute, second);
+                            start = start + startTime;
                         }
                         else
                         {
-                            //insert into datetime
-                            start = new DateTime(year, month, day);
+                            //get date
+                            DateTime.TryParseExact(line, "yyyyMMdd", enUS, DateTimeStyles.None, out start);
                         }
                     }
 
                     if (line.Contains("DTEND"))
                     {
-                        //set some variables
-                        int year = 0;
-                        int month = 0;
-                        int day = 0;
-                        int hour = 0;
-                        int minute = 0;
-                        int second = 0;
-
                         int last = line.LastIndexOf(":");
-                        line = line.Substring(last);
+                        line = line.Substring(last + 1);
 
                         //get date
-                        string temp = line.Substring(0, 4);
-                        year = Convert.ToInt32(temp);
-                        line = line.Substring(4);
-                        temp = line.Substring(0, 2);
-                        month = Convert.ToInt32(temp);
-                        line = line.Substring(2);
-                        temp = line.Substring(0, 2);
-                        day = Convert.ToInt32(temp);
+                        string temp = line.Substring(0, 8);
+                        DateTime.TryParseExact(temp, "yyyyMMdd", enUS, DateTimeStyles.None, out end);
 
-                        line = line.Substring(3);
+                        line = line.Substring(9);
 
                         //get time
-                        temp = line.Substring(0, 2);
-                        hour = Convert.ToInt32(temp);
-                        line = line.Substring(2);
-                        temp = line.Substring(0, 2);
-                        minute = Convert.ToInt32(temp);
-                        line = line.Substring(2);
-                        temp = line.Substring(0, 2);
-                        second = Convert.ToInt32(temp);
+                        temp = line.Substring(0, 6);
+                        if (temp.Contains('Z'))
+                        {
+                            temp = line.Substring(0, 5);
+                            temp = "0" + temp;
+                        }
+                        TimeSpan.TryParseExact(temp, @"hmmss", CultureInfo.InvariantCulture, TimeSpanStyles.None, out endTime);
 
                         //insert into datetime
-                        end = new DateTime(year, month, day, hour, minute, second);
+                        end = end + endTime;
                     }
 
                     //set duration, to 0 if no end date
@@ -216,8 +205,18 @@ namespace StudyN.Views
                     if (line.Contains("END:VEVENT"))
                     {
                         int room = rnd.Next(1000, 2000);
-                        CalendarManager calendarManager = new CalendarManager();
-                      //  calendarManager.CreateAppointment(id, name, start, duration, room);
+                        //CalendarManager calendarManager = new CalendarManager();
+                        //calendarManager.CreateAppointment(id, name, start, duration, room);
+                        if (end.Equals(zdate))
+                        {
+                            GlobalTaskData.TaskManager.AddTask(name, descript, start, 3, 0, 0);
+                        }
+                        else
+                        {
+                            GlobalTaskData.TaskManager.AddTask(name, descript, end, 3, 0, 0);
+                        }
+                        
+
                         start = new DateTime();
                         end = new DateTime();
                         duration = new TimeSpan();
@@ -226,22 +225,42 @@ namespace StudyN.Views
                     //read another line
                     line = sr.ReadLine();
                 }
+
+                //give success if so and tell user that they need to edit values
+                SuccessMessage();
+                WarningMessage();
+            }
+
+            //function to tell user to edit tasks
+            async private static void WarningMessage()
+            {
+                await App.Current.MainPage.DisplayAlert("Warning", "All pulled tasks hold an estimated time of 0 and a priority of 3.\n"
+                    + "Please change values in the task page.", "OK");
+            }
+
+            async private static void SuccessMessage()
+            {
+                await App.Current.MainPage.DisplayAlert("Success", "ICS file successfully imported", "OK");
             }
         }
 
+        
+        //pull ICS file from local files
         async private void Browse_Clicked(object sender, EventArgs e)
         {
             if (!string.IsNullOrEmpty(dirString))
             {
+                //take in string on .xaml page and move to char string
                 string jsonfiletext;
                 string[] file = FileManager.loadFile(dirString);
 
                 if (file.Length > 0)
                 {
+                    //out to filereader class to take string and find path and read string in file from path
                     jsonfiletext = File.ReadAllText(file[0]);
                     Console.WriteLine(jsonfiletext);
 
-                    //cal class to convert
+                    //call class to convert
                     GetAppointFromString convert = new GetAppointFromString(jsonfiletext);
                 }
                 else
